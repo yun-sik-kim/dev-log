1ac81bb4e70205ece8daad4c934a8c56544f17da
yun-sik-kim <90196456+yunsikk@users.noreply.github.com>
2024-04-09 23:50:53 +1000

    init

---
+++ src/app/main/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./main.module.css"
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.main_page}>
+        <div className={styles.grid_child}>Project 1</div>
+        <div className={styles.grid_child}>Project 2</div>
+        <div className={styles.grid_child}>Project 3</div>
+        <div className={styles.grid_child}>Project 4</div>
+        <div className={styles.grid_child}>Project 5</div>
+      </div>
+    </main>
+  );
+}
